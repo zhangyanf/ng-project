fd7185cdb4a386816162b2416d4f06a9e59a4f85
zengqiang <zengqiang@inhand.com.cn>
2017-04-26 15:15:28 +0800

    Symptom:更改登录页welcome标题，时钟调整左侧距离 BugID:N/A Root Cause:N/A Solution:N/A Note:N/A

--- DN4.1/src/main/webapp/config.js
+++ DN4.1/src/main/webapp/config.js
@@ -16,6 +16,7 @@
  */
 var setting = {
     host: "/smartATM",
+    //host: "/DN4.1",
     debugMode: false,
     CLIENT_ID: "17953450251798098136",
     CLIENT_SECRET: "08E9EC6793345759456CB8BAE52615F3",
